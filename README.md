0e56e49f0a7cae87128af41108ef5f2519058e32
divyaS09 <divsinind09@gmail.com>
2023-10-03 01:13:20 +0530

    commit-1

---
+++ README.md
@@ -0,0 +1,14 @@
+# AccommodationBookingWebsite
+Welcome to my  Accommodation Booking Website (Airbnb Clone)!
+
+This website is designed to facilitate seamless interactions between property owners and tenants. Whether you want to list your rental property, search for accommodations, or book your next stay, this platform is tailored to your needs!
+
+Key Features:
+-Streamlined listing of rental properties with rich details
+-Intuitive and efficient property search functionality
+-Effortless booking process for a stress-free experience
+
+Tech Stack:
+This Airbnb clone is built upon the MERN stack, comprising MongoDB, Express.js, React, and Node.js.
+
+Join our vibrant community of property owners and tenants today and experience the future of seamless rental accommodations. Happy travels!
---
+++ client/src/AccountNav.jsx
@@ -0,0 +1,42 @@
+import {Link, useLocation} from "react-router-dom";
+
+export default function AccountNav() {
+  const {pathname} = useLocation();
+  let subpage = pathname.split('/')?.[2];
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+  function linkClasses (type=null) {
+    let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
+    if (type === subpage) {
+      classes += ' bg-primary text-white';
+    } else {
+      classes += ' bg-gray-200';
+    }
+    return classes;
+  }
+  return (
+    <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+      <Link className={linkClasses('profile')} to={'/account'}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        </svg>
+        Profile
+      </Link>
+      <Link className={linkClasses('bookings')} to={'/account/bookings'}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </svg>
+        Bookings
+      </Link>
+      <Link className={linkClasses('places')} to={'/account/places'}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-house-check-fill" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z"/>
+  <path d="m8 3.293 4.712 4.712A4.5 4.5 0 0 0 8.758 15H3.5A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z"/>
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.707l.547.547 1.17-1.951a.5.5 0 1 1 .858.514Z"/>
+</svg>
+        Accommodations
+      </Link>
+    </nav>
+  );
+}
